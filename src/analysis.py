112ce212c09c863553a970ed27b21edb143bc9a0
Steven Baldasty <sbaldasty@gmail.com>
2026-05-31 23:05:01 -0400

    optimizations and comments

--- src/analysis.py
+++ src/analysis.py
@@ -88,25 +88,25 @@ def _symbolic_odds_ratio(a, b, c, d):
 
 
 def odds_ratio(tbl):
+    # Must have 2x2 table of noisy floats with postive finite observations
     tbl = as_noisy_float_array(tbl)
     assert tbl.shape == (2, 2)
     assert isfinite(asarray([float(value) for value in tbl.ravel()], dtype=float)).all()
 
+    # Compute totals and ratios as noisy floats for each group
     grp0_yes, grp0_no, grp1_yes, grp1_no = tbl.ravel()
-
     grp0_total = (grp0_yes + grp0_no).round_nearest()
     grp1_total = (grp1_yes + grp1_no).round_nearest()
     grp0_ratio = grp0_yes / (grp0_yes + grp0_no)
     grp1_ratio = grp1_yes / (grp1_yes + grp1_no)
 
-    builder = GraphBuilder(grp0_yes, grp0_no, grp1_yes, grp1_no)
+    # Chain symbolic binomial distributions for sampling uncertainty
     grp0_yes_draw = grp0_yes.round_nearest().resample(noise.binomial(grp0_total, grp0_ratio))
     grp1_yes_draw = grp1_yes.round_nearest().resample(noise.binomial(grp1_total, grp1_ratio))
-    builder.include_values(grp0_yes_draw, grp1_yes_draw)
-
     grp0_no_draw = grp0_total - grp0_yes_draw
     grp1_no_draw = grp1_total - grp1_yes_draw
 
+    # Basically deferred sample filtering
     valid = _preferred_value_expr(
         (grp0_total > 0)
         & (grp1_total > 0)
@@ -124,15 +124,14 @@ def odds_ratio(tbl):
     ratio_draw = (grp0_yes_draw * grp1_no_draw) / (grp0_no_draw * grp1_yes_draw)
     expr = Piecewise((_preferred_value_expr(ratio_draw), valid), (nan, True))
 
+    builder = GraphBuilder(grp0_yes, grp0_no, grp1_yes, grp1_no)
+    builder.include_values(grp0_yes_draw, grp1_yes_draw)
     root = builder.derived(definition=expr)
 
-    obs_valid = bool(
-        (grp0_yes > 0)
-        & (grp0_no > 0)
-        & (grp1_yes > 0)
-        & (grp1_no > 0))
-
+    # Observation for the result is calculation over inputs or NaN if nonpositive counts
+    obs_valid = bool((grp0_yes > 0) & (grp0_no > 0) & (grp1_yes > 0) & (grp1_no > 0))
     obs_or = float((grp0_yes * grp1_no) / (grp0_no * grp1_yes)) if obs_valid else nan
+
     return NoisyFloat.from_node(obs_or, root)
 
 
